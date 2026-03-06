2bd03794b8f9896a870ec6aaa031cf2c62b5ed23
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2026-03-06 11:51:08 +0800

    Establishes frontend routing and layout
    Initializes `react-router-dom` to manage application navigation.
    Defines a comprehensive set of routes for public access and distinct user roles (student, instructor, admin).
    Integrates `AuthLayout` to provide a consistent navigation structure for authenticated users, dynamically passing role-specific menu items.
    Replaces boilerplate `App.tsx` with the core application routing structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LoginPage from '@/pages/login/LoginPage'
+import AccountVerificationPage from '@/pages/verification/AccountVerificationPage'
+import LandingPage from '@/pages/LandingPage'
+import StudentDashboardPage from './pages/student/dashboard/StudentDashboardPage'
+import StudentAnalyticsPage from './pages/student/exams/StudentExamsPage'
+import InstructorDashboardPage from './pages/instructor/dashboard/InstructorDashboardPage'
+import InstructorExamsPage from './pages/instructor/exams/InstructorExamsPage'
+import InstructorListStudentPage from './pages/instructor/students/InstructorListStudentPage'
+import AdminDashboardPage from './pages/admin/dashboard/AdminDashboardPage'
+import AdminAccountsPage from './pages/admin/accounts/AdminAccountsPage'
+import AdminCoursesPage from './pages/admin/courses/AdminCoursesPage'
+import AuthLayout from './layout/AuthLayout'
+import ProfilePage from './pages/profile/ProfilePage'
+import { IconClipboardCheckFilled, IconDashboard, IconUsers, IconBook } from '@tabler/icons-react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const studentNavigatioItems = [
+  { label: "Dashboard", icon: IconDashboard, path: "/student" },
+  { label: "My Exams", icon: IconClipboardCheckFilled, path: "/student/exams" },
+]
 
+const instructorNavigationItems = [
+  { label: "Dashboard", icon: IconDashboard, path: "/instructor" },
+  { label: "Exams", icon: IconClipboardCheckFilled, path: "/instructor/exams" },
+  { label: "Students", icon: IconUsers, path: "/instructor/students" },
+]
+
+const adminNavigationItems = [
+  { label: "Dashboard", icon: IconDashboard, path: "/admin" },
+  { label: "Accounts", icon: IconUsers, path: "/admin/accounts" },
+  { label: "Courses", icon: IconBook, path: "/admin/courses" },
+]
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold text-center">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="bg-blue-700">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot-password" element={<AccountVerificationPage />} />
+
+        {/* Routes for student */}
+        <Route path="/student" element={<AuthLayout navigationItems={studentNavigatioItems}><StudentDashboardPage /></AuthLayout>} />
+        <Route path="/student/exams" element={<AuthLayout navigationItems={studentNavigatioItems}><StudentAnalyticsPage /></AuthLayout>} />
+        <Route path="/profile" element={<AuthLayout navigationItems={studentNavigatioItems}><ProfilePage /></AuthLayout>} />
+
+        {/* Routes for instructor */}
+        <Route path="/instructor" element={<AuthLayout navigationItems={instructorNavigationItems}><InstructorDashboardPage /></AuthLayout>} />
+        <Route path="/instructor/exams" element={<AuthLayout navigationItems={instructorNavigationItems}><InstructorExamsPage /></AuthLayout>} />
+        <Route path="/instructor/students" element={<AuthLayout navigationItems={instructorNavigationItems}><InstructorListStudentPage /></AuthLayout >} />
+
+        {/* Routes for admin */}
+        <Route path="/admin" element={<AuthLayout navigationItems={adminNavigationItems}><AdminDashboardPage /></AuthLayout>} />
+        <Route path="/admin/accounts" element={<AuthLayout navigationItems={adminNavigationItems}><AdminAccountsPage /></AuthLayout>} />
+        <Route path="/admin/courses" element={<AuthLayout navigationItems={adminNavigationItems}><AdminCoursesPage /></AuthLayout>} />
+      </Routes>
+    </Router>
   )
 }
 
