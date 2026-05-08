d5e43d6855138f5a17759ae21606d5e7f4362140
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2026-05-09 00:25:53 +0800

    Update some table attributes based on the latest Supabase config

--- src/lib/supabase/schema/exam-papers.sql
+++ src/lib/supabase/schema/exam-papers.sql
@@ -1,10 +1,10 @@
 create table public.exam_papers (
   paper_id uuid not null default gen_random_uuid (),
   exam_id uuid not null,
-  user_id uuid not null,
+  student_id uuid not null,
   actual_answers jsonb null,
   created_at timestamp with time zone not null default now(),
   constraint exam_papers_pkey primary key (paper_id),
-  constraint exam_papers_exam_id_fkey foreign key (exam_id) references exams (exam_id) on update cascade on delete cascade,
-  constraint exam_papers_user_id_fkey foreign key (user_id) references profiles (user_id) on update cascade on delete cascade
+  constraint exam_papers_exam_id_fkey foreign KEY (exam_id) references exams (exam_id) on update CASCADE on delete CASCADE,
+  constraint exam_papers_student_id_fkey foreign KEY (student_id) references profiles (user_id) on update CASCADE on delete CASCADE
 ) TABLESPACE pg_default;
--- src/lib/supabase/schema/feedbacks.sql
+++ src/lib/supabase/schema/feedbacks.sql
@@ -1,10 +1,10 @@
 create table public.feedbacks (
   feedback_id uuid not null default gen_random_uuid (),
   exam_id uuid not null,
-  user_id uuid not null,
+  student_id uuid not null,
   comment text not null,
-  created_at timestamp with time zone not null default now(),
+  message_at timestamp with time zone not null default now(),
   constraint feedbacks_pkey primary key (feedback_id),
   constraint feedbacks_exam_id_fkey foreign KEY (exam_id) references exams (exam_id) on update CASCADE on delete CASCADE,
-  constraint feedbacks_user_id_fkey foreign KEY (user_id) references profiles (user_id) on update CASCADE on delete CASCADE
+  constraint feedbacks_student_id_fkey foreign KEY (student_id) references profiles (user_id) on update CASCADE on delete CASCADE
 ) TABLESPACE pg_default;
