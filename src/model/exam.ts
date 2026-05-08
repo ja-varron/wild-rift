840462cac3d86f7c0fc116a036b5514a6e921d16
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2026-05-09 00:39:18 +0800

    Convert exam from class to interface

--- src/model/exam.ts
+++ src/model/exam.ts
@@ -1,124 +1,178 @@
-export class Exam {
-  private exam_id: string
-  private course_id: string
-  private exam_title: string
-  private exam_date: string
-  private total_items: number
-  private passing_rate: number
-  private topics: string[]
+// export class Exam {
+//   private exam_id: string
+//   private course_id: string
+//   private exam_title: string
+//   private exam_date: string
+//   private total_items: number
+//   private passing_rate: number
+//   private topics: string[]
 
-  constructor(
-    exam_id: string | Exam,
-    course_id?: string,
-    exam_title?: string,
-    exam_date?: string,
-    total_items?: number,
-    passing_rate?: number,
-    topics?: string[]
-  ) {
-    if (exam_id instanceof Exam) {
-      this.exam_id = exam_id.exam_id
-      this.course_id = exam_id.course_id
-      this.exam_title = exam_id.exam_title
-      this.exam_date = exam_id.exam_date
-      this.total_items = exam_id.total_items
-      this.passing_rate = exam_id.passing_rate
-      this.topics = exam_id.topics
-    } else {
-      this.exam_id = exam_id
-      this.course_id = course_id!
-      this.exam_title = exam_title!
-      this.exam_date = exam_date!
-      this.total_items = total_items!
-      this.passing_rate = passing_rate!
-      this.topics = topics || []
-    }
-  }
+//   constructor(
+//     exam_id: string | Exam,
+//     course_id?: string,
+//     exam_title?: string,
+//     exam_date?: string,
+//     total_items?: number,
+//     passing_rate?: number,
+//     topics?: string[]
+//   ) {
+//     if (exam_id instanceof Exam) {
+//       this.exam_id = exam_id.exam_id
+//       this.course_id = exam_id.course_id
+//       this.exam_title = exam_id.exam_title
+//       this.exam_date = exam_id.exam_date
+//       this.total_items = exam_id.total_items
+//       this.passing_rate = exam_id.passing_rate
+//       this.topics = exam_id.topics
+//     } else {
+//       this.exam_id = exam_id
+//       this.course_id = course_id!
+//       this.exam_title = exam_title!
+//       this.exam_date = exam_date!
+//       this.total_items = total_items!
+//       this.passing_rate = passing_rate!
+//       this.topics = topics || []
+//     }
+//   }
 
-  // Exam ID is read-only after creation
-  get getExamId() {
-    return this.exam_id
-  }
+//   // Exam ID is read-only after creation
+//   get getExamId() {
+//     return this.exam_id
+//   }
 
-  // Course ID is read-only after creation
-  get getCourseId() {
-    return this.course_id
-  }
+//   // Course ID is read-only after creation
+//   get getCourseId() {
+//     return this.course_id
+//   }
 
-  // Exam title can be updated after creation
-  get getExamTitle() {
-    return this.exam_title
-  }
+//   // Exam title can be updated after creation
+//   get getExamTitle() {
+//     return this.exam_title
+//   }
 
-  set setExamTitle(title: string) {
-    this.exam_title = title
-  }
+//   set setExamTitle(title: string) {
+//     this.exam_title = title
+//   }
 
-  // Exam date can be updated after creation
-  get getExamDate() {
-    return this.exam_date
-  }
+//   // Exam date can be updated after creation
+//   get getExamDate() {
+//     return this.exam_date
+//   }
 
-  set setExamDate(date: string) {
-    this.exam_date = date
-  }
+//   set setExamDate(date: string) {
+//     this.exam_date = date
+//   }
 
-  // Total items can be updated after creation
-  get getTotalItems() {
-    return this.total_items
-  }
+//   // Total items can be updated after creation
+//   get getTotalItems() {
+//     return this.total_items
+//   }
 
-  set setTotalItems(items: number) {
-    this.total_items = items
-  }
+//   set setTotalItems(items: number) {
+//     this.total_items = items
+//   }
 
-  // Passing rate can be updated after creation
-  get getPassingRate() {
-    return this.passing_rate
-  }
+//   // Passing rate can be updated after creation
+//   get getPassingRate() {
+//     return this.passing_rate
+//   }
 
-  set setPassingRate(rate: number) {
-    this.passing_rate = rate
-  }
+//   set setPassingRate(rate: number) {
+//     this.passing_rate = rate
+//   }
 
-  // Topics can be updated after creation
-  get getTopics() {
-    return this.topics
-  }
+//   // Topics can be updated after creation
+//   get getTopics() {
+//     return this.topics
+//   }
 
-  set setTopics(topics: string[]) {
-    this.topics = topics
-  }
+//   set setTopics(topics: string[]) {
+//     this.topics = topics
+//   }
+// }
+
+// export class ExamResult {
+//   id: number;
+//   title: string;
+//   date: string;
+//   score: number;
+//   total_items: number;
+//   passed: boolean;
+//   attempted: boolean;
+
+//   constructor({ id, title, date, score, total_items, passed, attempted }: {
+//     id: number;
+//     title: string;
+//     date: string;
+//     score: number;
+//     total_items: number;
+//     passed: boolean;
+//     attempted?: boolean;
+//   }) {
+//     this.id = id;
+//     this.title = title;
+//     this.date = date;
+//     this.score = score;
+//     this.total_items = total_items;
+//     this.passed = passed;
+//     this.attempted = attempted ?? true;
+//   }
+
+//   get percentage(): number {
+//     return Math.round((this.score / this.total_items) * 100);
+//   }
+// }
+
+export interface Exam {
+  scannedPapers: ScannedPaper[];
+  exam_id: string
+  course: { course_id: string; course_name: string } | null;
+  profile: { first_name: string; last_name: string } | null;
+  exam_title: string
+  exam_date: string
+  total_items: number
+  passing_rate: number
+  created_by: string
+  created_at: string
+  topics: string[]
 }
 
 export class ExamResult {
-  id: number;
-  title: string;
-  date: string;
-  score: number;
-  total_items: number;
-  passed: boolean;
-  attempted: boolean;
+  id: number
+  title: string
+  date: string
+  score: number
+  total_items: number
+  passed: boolean
+  attempted: boolean
 
-  constructor({ id, title, date, score, total_items, passed, attempted }: {
-    id: number;
-    title: string;
-    date: string;
-    score: number;
-    total_items: number;
-    passed: boolean;
-    attempted?: boolean;
+  constructor({
+    id,
+    title,
+    date,
+    score,
+    total_items,
+    passed,
+    attempted,
+  }: {
+    id: number
+    title: string
+    date: string
+    score: number
+    total_items: number
+    passed: boolean
+    attempted?: boolean
   }) {
-    this.id = id;
-    this.title = title;
-    this.date = date;
-    this.score = score;
-    this.total_items = total_items;
-    this.passed = passed;
-    this.attempted = attempted ?? true;
+    this.id = id
+    this.title = title
+    this.date = date
+    this.score = score
+    this.total_items = total_items
+    this.passed = passed
+    this.attempted = attempted ?? true
   }
 
   get percentage(): number {
-    return Math.round((this.score / this.total_items) * 100);
+    return Math.round((this.score / this.total_items) * 100)
   }
 }
